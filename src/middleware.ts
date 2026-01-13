1456c8a5f96962f40f035a39aed6d93b116bba07
Pimous34 <benjamin.rigouste@gmail.com>
2026-01-13 09:22:28 +0100

    ok

--- src/middleware.ts
+++ src/middleware.ts
@@ -63,7 +63,16 @@ export async function middleware(request: NextRequest) {
     }
   )
 
-  await supabase.auth.getUser()
+  const {
+    data: { user },
+  } = await supabase.auth.getUser()
+
+  if (!user && !request.nextUrl.pathname.startsWith('/auth')) {
+    // Redirect to login page if not logged in
+    const url = request.nextUrl.clone()
+    url.pathname = '/auth'
+    return NextResponse.redirect(url)
+  }
 
   return response
 }
